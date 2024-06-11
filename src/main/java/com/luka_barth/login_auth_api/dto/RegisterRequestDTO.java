5b520666ea8ef10da5abc3b2676b5687d5473bbb
Luka <lukadbarth@hotmail.com>
2024-06-11 11:36:07 -0300

    Arquivos do projeto

---
+++ src/main/java/com/luka_barth/login_auth_api/dto/RegisterRequestDTO.java
@@ -0,0 +1,4 @@
+package com.luka_barth.login_auth_api.dto;
+
+public record RegisterRequestDTO(String name, String email, String password) {
+}
---
+++ src/main/java/com/luka_barth/login_auth_api/dto/ResponseDTO.java
@@ -0,0 +1,4 @@
+package com.luka_barth.login_auth_api.dto;
+
+public record ResponseDTO(String name, String token) {
+}
